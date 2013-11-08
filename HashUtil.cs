c10b6676d47b3397a2784b85089c068f26f2315a
Sanders DeNardi <sdenardi@relsci.com>
2013-11-08 11:30:10 -0500

    Add files + update filenames in readme

---
+++ HashUtil.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Security.Cryptography;
+
+public class HashUtil
+{
+    [SqlFunction(IsDeterministic = true)]
+    public static SqlBinary GetHash(SqlString algorithm, SqlBytes src)
+    {
+        if (src.IsNull)
+            return null;
+
+        switch (algorithm.Value.ToUpperInvariant())
+        {
+            case "MD5":
+                return new SqlBinary(MD5.Create().ComputeHash(src.Stream));
+            case "SHA1":
+                return new SqlBinary(SHA1.Create().ComputeHash(src.Stream));
+            case "SHA256":
+                return new SqlBinary(SHA256.Create().ComputeHash(src.Stream));
+            case "SHA512":
+                return new SqlBinary(SHA512.Create().ComputeHash(src.Stream));
+            default:
+                throw new ArgumentException("HashType", "Unrecognized hashtype: " + algorithm.Value);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,14 +3,14 @@ sql-hashing-clr
 
 SQL Server CLR that implements hashing for 4 different algorithms that does not have SQL Server's 8000 byte input limitation. This project also includes a function that uses both the HASHBYTES() function and this CLR based on the length of the input.
 
-###clr.cs
+###HashUtil.cs
 The actual CLR class.
 
-###function.sql
+###GetHashHybrid.function.sql
 The function that either calls SQL Server's HASHBYTES() function or the CLR based on the length of the input.
 
-###sub8000-benchmark.sql
+###benchmark-clr.script.sql
 This script evaluates the CPU usage of the CLR vs. HASHBYTES(). Detailed information can be found in the comments for the script.
 
-###hybrid-benchmark.sql
+###benchmark-hybrid.script.sql
 This script evaluates the CPU usage of the CLR vs. the hybrid function which uses a combination of the CLR and HASHBYTES(). Detailed information can be found in the script.
